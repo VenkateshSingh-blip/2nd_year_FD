51d1d860ad0d521143d7e6df862cf9ef278a5a58
Venkatesh Singh <venkateshsingh455@gmail.com>
2026-08-21 14:20:23 +0530

    js day1

---
+++ loops/for.js
@@ -0,0 +1,15 @@
+//for(initialization; condition; increment/decrement){
+// statement}
+let sum = 0;
+for(let i =0; i<=9; i++, sum+=i);
+console.log(sum);
+let x=4,y=0,z;
+while(x>=0){
+    x--;
+    y++;
+    if(x==y)BroadcastChannel;
+    else 
+        console.log(x,y);
+}
+let arr=[10,20,30,40,50];
+for (let i=0;i<5;i++){
